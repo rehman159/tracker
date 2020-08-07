75f31dff1fb1423fe4dc6f889bf4fe2f3089dbfc
rehman159 <rehman_uetpesh@yahoo.com>
2020-08-07 06:10:20 +0500

    successfully did inheritance with prototype property

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hi");
+var Person = function (name, age) {
+  this.name = name;
+  this.age = age;
+};
+Person.prototype.calculateYear_of_Birth = function () {
+  return 2020 - this.age;
+};
+var rehman = new Person("Rehman Khan", 30);
+console.log(rehman.name);
+
+console.log(rehman.calculateYear_of_Birth());
